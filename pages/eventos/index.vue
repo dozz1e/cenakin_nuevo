51a8940daf664b2f5d5636e26f05c34591197b41
Dozz1e <darkozzie@gmail.com>
2025-07-29 13:07:09 -0400

    Página eventos

---
+++ pages/eventos/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Cenakin OTEC Cursos Viña del Mar, Chile",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Un nuevo método de enseñanza para distintos cursos, con materiales incluidos, docentes kinesiólogos y calidad de servicio Viña del Mar, Chile.",
+        },
+        { property: "og:type", content: "website" },
+        {
+          property: "og:title",
+          content:
+            "Cenakin Cursos Online y Presenciales en Viña del Mar, Chile",
+        },
+        {
+          property: "og:description",
+          content:
+            "Un nuevo método de enseñanza para distintos cursos, con materiales incluidos, docentes kinesiólogos y calidad de servicio Viña del Mar, Chile.",
+        },
+        { property: "og:url", content: "https://www.cenakin.cl/cursos" },
+      ],
+    };
+  },
+};
+</script>
